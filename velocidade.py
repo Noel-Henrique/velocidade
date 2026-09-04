db1df68cbeee42bd49bf97871f496fbc1b2d9399
Manoel <manoelhenrique1214@gmail.com>
2026-09-04 20:19:29 -0300

    velocidade

--- velocidade.py
+++ velocidade.py
@@ -1,6 +1,24 @@
-g= -9.8
-for i in range (11):
-    t=i
-    v=g*t
-    print("t=",t,"s v=",v,"m/s")
+g=-9.8 #em m/s²
+v0=25 
+for i in range (51):
+    t=i*0.1
+    v=v0 + g*t
+    print (t, v)
+    if v <=0:
+        break
+    print ("A velocidade chegou a zero ou ficou negativa em t = ", t, " s")
 
+
+
+'''
+if v <= 0:
+    print ("A velocidade chegou a zero ou ficou negativa. ")
+for i in range(21):
+    t = i * 0.1
+    v = v0 + a * t
+    print (t, v)
+    if v <= 0:
+        print ("Atingiu a altura máxima.")
+        break
+'''
+    
